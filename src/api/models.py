1c1377c65e680efbd77c57ae2c35b391c5966ce9
Logan Havens <153969446+Logan-Havens@users.noreply.github.com>
2024-05-14 00:25:41 +0000

    added event table to the database

--- src/api/models.py
+++ src/api/models.py
@@ -17,13 +17,6 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     dob = db.Column(db.Date, nullable=False)
 
-class Favorites(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    location_id = db.Column(db.Integer, db.ForeignKey('location_id'))
-    event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
-    
-
     def __repr__(self):
         return f'<User {self.email}>'
 
@@ -39,6 +32,52 @@ class Favorites(db.Model):
             "email": self.email,
             "dob": self.dob.isoformat(),
             # Do not serialize the password, it's a security breach
+        }
+    
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    location_id = db.Column(db.Integer, db.ForeignKey('location_id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
+    date_of_event = db.Column(db.Date, nullable=False)
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    date_of_event = db.Column(db.Date, nullable=False)
+    street = db.Column(db.String(120), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    state = db.Column(db.String(50), nullable=False)
+    zip_code = db.Column(db.String(20), nullable=False)
+    country = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f'<Event {self.name} on {self.date_of_event}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "date_of_event": self.date_of_event.isoformat(),  # Serialize date as ISO format string
+            "user_id": self.user_id,
+            "street": self.street,
+            "city": self.city,
+            "state": self.state,
+            "zip_code": self.zip_code,
+            "country": self.country
+        }
+    
+
+    def __repr__(self):
+        return f'<Event {self.name} on {self.date_of_event}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "date_of_event": self.date_of_event.isoformat(),  # Serialize date as ISO format string
+            "user_id": self.user_id
         }
 
 # Ensure that you create the tables
