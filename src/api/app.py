715cd36e8295b58fa0f8792877d71f8c03181b90
Logan Havens <153969446+Logan-Havens@users.noreply.github.com>
2024-05-20 22:11:19 +0000

    getting started on methods

---
+++ src/api/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_cors import CORS
+from datetime import datetime
+from models import db, User, Favorites 
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this in production
+
+db.init_app(app)
+bcrypt.init_app(app)
+jwt = JWTManager(app)
+CORS(app)
+
