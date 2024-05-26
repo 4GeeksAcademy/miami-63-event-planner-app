3a9e36066db3146e87d4ef8c56ec4126ef2cf9e0
Ernesto Martinez <61639551+ErnestWarhead@users.noreply.github.com>
2024-05-26 23:19:29 +0000

    added end points, authentication, integration with Ticketmaster API, hashed passwords and misc

--- src/app.py
+++ src/app.py
@@ -4,66 +4,62 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 import os
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_swagger import swagger
-from datetime import datetime
 from api.utils import APIException, generate_sitemap
-from api.models import db, User, Favorites 
+from api.models import db, User, Favorites
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 
-# from models import Person
-
+# Determine the environment
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
-static_file_dir = os.path.join(os.path.dirname(
-    os.path.realpath(__file__)), '../public/')
+static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-# database condiguration
+# Database configuration
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
-        "postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-MIGRATE = Migrate(app, db, compare_type=True)
-db.init_app(app)
+app.config['JWT_SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-# add the admin
+# Initialize extensions
+db.init_app(app)
+MIGRATE = Migrate(app, db, compare_type=True)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+CORS(app)
+
+# Add the admin
 setup_admin(app)
 
-# add the admin
+# Add commands
 setup_commands(app)
 
-# Add all endpoints form the API with a "api" prefix
+# Register the API blueprint
 app.register_blueprint(api, url_prefix='/api')
 
 # Handle/serialize errors like a JSON object
-
-
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
-
-
+# Generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     if ENV == "development":
         return generate_sitemap(app)
     return send_from_directory(static_file_dir, 'index.html')
 
-# any other endpoint will try to serve it like a static file
-
-
+# Serve any other file
 @app.route('/<path:path>', methods=['GET'])
 def serve_any_other_file(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
@@ -72,8 +68,7 @@ def serve_any_other_file(path):
     response.cache_control.max_age = 0  # avoid cache memory
     return response
 
-
-# this only runs if `$ python src/main.py` is executed
+# Run the application
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3001))
     app.run(host='0.0.0.0', port=PORT, debug=True)
