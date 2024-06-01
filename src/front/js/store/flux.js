fa3e96db2e04c56d7a62288953f8e3d448ab97dc
Logan Havens <153969446+Logan-Havens@users.noreply.github.com>
2024-06-01 17:09:14 +0000

    added login, newUser function

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,58 +1,46 @@
-import React, { createContext, useReducer, useContext } from 'react';
 
-
-const initialState = {
-    user: null,
-    favorites: [],
+const getState = ({ getStore, getActions, setStore }) => {
+    return {
+        store: {
+            token: null,
+            email: null,
+            events: [],
+            favorites: [],
+            currentIndex: 0,
+        },
+        actions: {
+            newUser: async (email, password, location) => {
+                try {
+                    const resp = await fetch(process.env.BACKEND_URL + "/api/signup", {
+                        method: "POST",
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify({ email, password, location }),
+                    });
+                    const data = await resp.json();
+                    return { ok: resp.ok, msg: data.msg };
+                } catch (error) {
+                    console.log("Error creating new user", error);
+                    return { ok: false, msg: "Error creating new user" };
+                }
+            },
+            login: async(email, password) => {
+                try {
+                    const resp = await fetch(process.env.BACKEND_URL + "/api/login", {
+                        method: "POST",
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify({email,password}),
+                    });
+                    const data = await resp.json();
+                    if (resp.ok) {
+                        localStorage.setItem("token", data.token);
+                        localStorage.setItem("email", email);
+                        setStore({ token: data.token, email});
+                    }
+                    return { ok: resp.ok, msg: data.msg};
+                } catch (error){
+                    console.log("Error logging in", error);
+                    return {ok: false, msg; "Error logging in"};
+            }
+        },
+    },
 };
-
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'SET_USER':
-            return {
-                ...state,
-                user: action.payload
-            };
-        case 'LOGOUT_USER':
-            return {
-                ...state,
-                user: null
-            };
-        case 'ADD_FAVORITE':
-            return {
-                ...state,
-                favorites: [...state.favorites, action.payload]
-            };
-        case 'REMOVE_FAVORITE':
-            return {
-                ...state,
-                favorites: state.favorites.filter(fav => fav.id !== action.payload)
-            };
-        case 'SET_FAVORITES':
-            return {
-                ...state,
-                favorites: action.payload
-            };
-        default:
-            return state;
-    }
-};
-
-
-const FluxContext = createContext();
-
-
-export const useFlux = () => useContext(FluxContext);
-
-
-export const FluxProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
-
-    return (
-        <FluxContext.Provider value={{ state, dispatch }}>
-            {children}
-        </FluxContext.Provider>
-    );
-};
-
